ef357ee56872f1ffe72282241135a0c3df875fc3
Pär Nygren <par.a.nygren@gmail.com>
2017-11-16 05:43:15 +0100

    Using IApplicationBuilder

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -18,12 +19,36 @@ namespace OdeToFood
         public void Configure(
             IApplicationBuilder app,
             IHostingEnvironment env,
-            IGreeter greeter)
+            IGreeter greeter,
+            ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            
+            app.Use(next =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                return async context =>
+                {
+                    logger.LogInformation("Request incoming");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit!!");
+                        logger.LogInformation("Request handled");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Response outgoing");
+                    }
+                };
+            });
+
+            app.UseWelcomePage(new WelcomePageOptions()
+            {
+                Path = "/wp"
+            });
 
             app.Run(async (context) =>
             {
