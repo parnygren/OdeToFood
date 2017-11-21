4aa2869c5eace9546344c0f3bbadffc427793dc9
Pär Nygren <par.a.nygren@gmail.com>
2017-11-21 09:09:32 +0100

    Model Validation with Data Annotations

--- OdeToFood/ViewModels/RestaurantEditModel.cs
+++ OdeToFood/ViewModels/RestaurantEditModel.cs
@@ -1,9 +1,11 @@
-﻿using OdeToFood.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using OdeToFood.Models;
 
 namespace OdeToFood.ViewModels
 {
     public class RestaurantEditModel
     {
+        [Required, MaxLength(80)]
         public string Name { get; set; }
         public CuisineType Cuisine { get; set; }
     }
